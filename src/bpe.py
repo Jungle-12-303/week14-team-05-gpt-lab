d30f752f9fbef337c4c3a2e18ff2a296bfe61ab0
binny <binny0x00@gmail.com>
2026-05-26 16:59:40 +0900

    feat: BPETokenizer _init_special_tokens 구현

--- src/bpe.py
+++ src/bpe.py
@@ -15,8 +15,8 @@ UNK_TOKEN = "<unk>"
 BOS_TOKEN = "<bos>"
 EOS_TOKEN = "<eos>"
 
-SPECIAL_TOKENS = [PAD_TOKEN, UNK_TOKEN, BOS_TOKEN, EOS_TOKEN]
-SPECIAL_IDS = {token: idx for idx, token in enumerate(SPECIAL_TOKENS)}
+SPECIAL_TOKENS = [PAD_TOKEN, UNK_TOKEN, BOS_TOKEN, EOS_TOKEN]   
+SPECIAL_IDS = {token: idx for idx, token in enumerate(SPECIAL_TOKENS)}  # enumberate: 리스트를 순회하면서 (인덱스, 값) 꺼냄
 BYTE_OFFSET = len(SPECIAL_TOKENS)
 NUM_BYTES = 256
 
@@ -33,17 +33,22 @@ class BPETokenizer:
 
     def __init__(self, vocab_size: int = 3000):
         self.vocab_size = vocab_size
-        self.id_to_token = {}
-        self.token_to_id = {}
+        self.id_to_token = {}   # id -> token
+        self.token_to_id = {}   # token -> id
         self.merges = []
 
     def _init_special_tokens(self):
-        """
-        TODO:
-        1. 특수 토큰 4개를 고정 ID 0~3에 등록합니다.
-        2. byte 0~255를 ID 4~259에 bytes([byte_value]) 형태로 등록합니다.
-        """
-        raise NotImplementedError("_init_special_tokens를 구현하세요.")
+        # 특수 토큰 4개를 고정 ID 0~3에 등록합니다.
+        self.id_to_token = {idx: token for idx, token in enumerate(SPECIAL_TOKENS)}
+        self.token_to_id = SPECIAL_IDS.copy()
+
+        # byte 0~255를 ID 4~259에 bytes([byte_value]) 형태로 등록합니다.
+        for byte_value in range(NUM_BYTES):
+            token_id = byte_value + BYTE_OFFSET
+            token = bytes([byte_value])
+            
+            self.id_to_token[token_id] = token
+            self.token_to_id[token] = token_id
 
     def get_pad_id(self):
         """padding 토큰 ID."""
@@ -89,14 +94,15 @@ class BPETokenizer:
 
     def encode(self, text: str, add_bos_eos: bool = False) -> list[int]:
         """
-        TODO: 문자열을 token ID 리스트로 변환합니다.
+        문자열을 token ID 리스트로 변환합니다.
 
         구현 힌트:
         - 먼저 UTF-8 byte ID 리스트를 만듭니다.
         - train/load에서 얻은 merge rule을 학습 순서대로 적용합니다.
         - add_bos_eos=True이면 앞뒤에 bos/eos ID를 붙입니다.
         """
-        raise NotImplementedError("BPETokenizer.encode를 구현하세요.")
+
+        
 
     def decode(self, ids: list[int], skip_special: bool = True) -> str:
         """
